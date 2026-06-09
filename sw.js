f4e41eae48bdf2ebc70cbf7f3bb98cca45597c28
jackkeluk <91089063+jackkeluk@users.noreply.github.com>
2026-06-09 14:15:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,19 @@
-const CACHE_NAME = 'french-flashcards-v1';
+const CACHE_NAME = 'french-flashcards-v2';
 const ASSETS = [
+  './',
   './index.html',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
-
+ 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
-
+ 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
@@ -21,9 +22,9 @@ self.addEventListener('activate', e => {
   );
   self.clients.claim();
 });
-
+ 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
   );
 });
